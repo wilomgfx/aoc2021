8256839cc37958d505584024aa61b8cee6eedeaa
William Cantin <william@hugocantin.info>
2021-12-04 15:25:01 -0500

    Cols done, rows WIP

--- src/day4/index.ts
+++ src/day4/index.ts
@@ -8,8 +8,7 @@ const prepareInput = (rawInput: RawInput) => rawInput
 
 const input = prepareInput(readInput())
 
-const getNextDraws = (draws: number[], drawNumber: number) => {
-  // console.log(`drawNumber ${drawNumber}`)
+const getNextDraw = (draws: number[], drawNumber: number) => {
   const nextSlice = 6 * (drawNumber - 1)
   if (drawNumber === 1) {
     return draws.slice(0, 5)
@@ -17,8 +16,47 @@ const getNextDraws = (draws: number[], drawNumber: number) => {
   if (drawNumber === 2) {
     return draws.slice(5, drawNumber * 6 - 1)
   }
-  console.log(`nextSlice ${nextSlice}`)
   return draws.slice(nextSlice - 1, nextSlice + 5)
+}
+
+const checkForValidNumbersInRow = (
+  draw: number[],
+  board: (string | undefined)[],
+  boardIndex: number,
+) => {
+  const rowsMap: Map<number, { [x: number]: boolean }[]> = new Map()
+  const rows = []
+  for (const [index, row] of board.entries()) {
+    const parsedRow = row!.split(" ").filter(Boolean).map(Number)
+    const rowNum = parsedRow![index]
+    if (draw.includes(rowNum)) {
+      rows.push({ [rowNum]: true })
+    }
+    if (!rowsMap.has(boardIndex)) rowsMap.set(boardIndex, rows)
+  }
+  console.log(rowsMap)
+  return rowsMap
+}
+
+const checkForValidNumbersInCol = (
+  draw: number[],
+  board: (string | undefined)[],
+  boardIndex: number,
+) => {
+  const colsMap: Map<number, { [x: number]: boolean }[]> = new Map()
+  const cols = []
+  for (const [index, row] of board.entries()) {
+    const parsedRow = row!.split(" ").filter(Boolean).map(Number)
+
+    const col = parsedRow![index]
+    console.log(col)
+    if (draw.includes(col)) {
+      cols.push({ [col]: true })
+    }
+    if (!colsMap.has(boardIndex)) colsMap.set(boardIndex, cols)
+  }
+  console.log(colsMap)
+  return colsMap
 }
 
 const goA = (input: RawInput) => {
@@ -35,16 +73,23 @@ const goA = (input: RawInput) => {
     })
     .filter(Boolean)
   const playBoards = _.chunk(boards, 5)
-  const firstDraw = getNextDraws(draws, 1)
-  console.log(
-    firstDraw,
-    getNextDraws(draws, 2),
-    getNextDraws(draws, 3),
-    getNextDraws(draws, 4),
-    getNextDraws(draws, 5),
-  )
-  for (const board of playBoards) {
+  // const firstDraw = getNextDraw(draws, 1)
+  // console.log(
+  //   firstDraw,
+  //   getNextDraws(draws, 2),
+  //   getNextDraws(draws, 3),
+  //   getNextDraws(draws, 4),
+  //   getNextDraws(draws, 5),
+  //   getNextDraws(draws, 6),
+  // )
+  let gameNumber = 1
+  for (const [boardIndex, board] of playBoards.entries()) {
     // console.log(board)
+    const nextDraw = getNextDraw(draws, gameNumber)
+    // console.log(nextDraw)
+    checkForValidNumbersInRow(nextDraw, board, boardIndex)
+    // checkForValidNumbersInCol(nextDraw, board, boardIndex)
+    gameNumber++
   }
   return
 }
